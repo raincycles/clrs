86efd03e94b07c0f08d4d817582edddf23eaf0f9
raincycles <143883317+raincycles@users.noreply.github.com>
2024-06-25 19:30:57 +0200

    Add binary search

---
+++ clrs/binary_search.py
@@ -0,0 +1,48 @@
+def binary_search(data: list[int], start: int, end: int, target: int) -> int:
+    low = start
+    high = end - 1
+
+    while low <= high:
+        mid = (high + low) // 2
+
+        if data[mid] < target:
+            low = mid + 1
+        elif data[mid] > target:
+            high = mid - 1
+        else:
+            return mid
+
+    return -1
+
+
+def binary_search_rec(data: list[int], start: int, end: int, target: int) -> int:
+    if start > end:
+        return -1
+
+    mid = (end + start) // 2
+
+    if data[mid] < target:
+        return binary_search_rec(data, mid + 1, end, target)
+
+    if data[mid] > target:
+        return binary_search_rec(data, start, mid - 1, target)
+
+    return mid
+
+
+def binary_search_leftmost(data: list[int], start: int, end: int, target: int) -> int:
+    low = start
+    high = end
+
+    while low < high:
+        mid = (high + low) // 2
+        
+        if data[mid] < target:
+            low = mid + 1
+        else:
+            high = mid
+
+    if data[low] == target:
+        return low
+
+    return -1
---
+++ tests/binary_search_test.py
@@ -0,0 +1,32 @@
+import pytest
+
+from clrs.binary_search import (
+    binary_search,
+    binary_search_leftmost,
+    binary_search_rec,
+)
+
+
+test_params = [
+    ([18, 13, 33, 34, 92, 97], 0, 5, 33, 2),
+    ([15, 20, 41, 54, 70, 82], 0, 5, 99, -1),
+    ([20, 28, 45, 80, 93, 95], 1, 4, 20, -1),
+]
+
+
+@pytest.mark.parametrize("test_data,start,end,target,expected", test_params)
+def test_binary_search(test_data, start, end, target, expected):
+    idx = binary_search(test_data, start, end, target)
+    assert idx == expected
+
+
+@pytest.mark.parametrize("test_data,start,end,target,expected", test_params)
+def test_binary_search_rec(test_data, start, end, target, expected):
+    idx = binary_search_rec(test_data, start, end, target)
+    assert idx == expected
+
+
+@pytest.mark.parametrize("test_data,start,end,target,expected", test_params)
+def test_binary_search_leftmost(test_data, start, end, target, expected):
+    idx = binary_search_leftmost(test_data, start, end, target)
+    assert idx == expected
