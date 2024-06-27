984ce5179f50a55e1aa6e9c483eabe5d34f572b1
raincycles <143883317+raincycles@users.noreply.github.com>
2024-06-27 14:19:14 +0200

    Fix index usage in binary_search_rec

--- clrs/binary_search.py
+++ clrs/binary_search.py
@@ -16,16 +16,19 @@ def binary_search(data: list[int], start: int, end: int, target: int) -> int:
 
 
 def binary_search_rec(data: list[int], start: int, end: int, target: int) -> int:
-    if start > end:
+    low = start
+    high = end - 1
+
+    if low > high:
         return -1
 
-    mid = start + (end - start) // 2
+    mid = low + (high - low) // 2
 
     if data[mid] < target:
         return binary_search_rec(data, mid + 1, end, target)
 
     if data[mid] > target:
-        return binary_search_rec(data, start, mid - 1, target)
+        return binary_search_rec(data, start, mid, target)
 
     return mid
 
