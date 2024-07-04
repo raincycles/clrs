aa130d40602c5396baeba23616f05106ad3a1aac
raincycles <143883317+raincycles@users.noreply.github.com>
2024-07-04 20:02:34 +0200

    Add `min_value` parameter to counting sort

--- tests/counting_sort_test.py
+++ tests/counting_sort_test.py
@@ -4,18 +4,18 @@ from clrs.counting_sort import counting_sort
 
 
 @pytest.mark.parametrize(
-    "input_list,max_value,expected",
+    "input_list,min_value,max_value,expected",
     [
-        ([], 1, []),
-        ([1], 1, [1]),
-        ([5, 5, 5], 5, [5, 5, 5]),
-        ([3, 4, 2, 5], 10, [2, 3, 4, 5]),
-        ([1, 2, 3, 4, 5], 5, [1, 2, 3, 4, 5]),
-        ([5, 4, 3, 2, 1], 5, [1, 2, 3, 4, 5]),
-        ([4, 5, 5, 2, 1, 4, 2], 5, [1, 2, 2, 4, 4, 5, 5]),
+        ([], 0, 0, []),
+        ([1], 1, 1, [1]),
+        ([5, 5, 5], 5, 5, [5, 5, 5]),
+        ([3, 4, 2, 5], 0, 10, [2, 3, 4, 5]),
+        ([1, 2, 3, 4, 5], 1, 5, [1, 2, 3, 4, 5]),
+        ([5, 4, 3, 2, 1], 1, 5, [1, 2, 3, 4, 5]),
+        ([4, 5, 5, 2, 1, 4, 2], 1, 5, [1, 2, 2, 4, 4, 5, 5]),
     ],
 )
-def test_counting_sort(input_list, max_value, expected):
+def test_counting_sort(input_list, min_value, max_value, expected):
     a = list(input_list)
-    counting_sort(a, max_value)
+    counting_sort(a, min_value, max_value)
     assert a == expected
